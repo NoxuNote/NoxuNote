a58c976ebfee27e356210c2f326a3a5df039be3f
Léo <rolland.leo@orange.fr>
2019-09-04 18:23:58 +0200

    FIX: buildResources misspelled and squirrel rm

--- app/main.ts
+++ app/main.ts
@@ -14,7 +14,7 @@ export {};
  */
 
 // Verbose
-const DEBUG = true
+const DEBUG = false
 
 // Importing electron library
 const { Menu, dialog, app, BrowserWindow, ipcMain  } = require('electron')
@@ -24,78 +24,6 @@ import { ipcRenderer } from "electron";
 // Importing external modules
 const fs				= require('fs-extra')
 const homedir		= require('os').homedir()
-
-/***************************************************************************************************
- *                                         SQUIRREL SETUP                                          *
- ***************************************************************************************************/
-
-// Catch squirrel events 
-if (handleSquirrelEvent(app)) {
-	//return;
-}
-function handleSquirrelEvent(application: Electron.App) {
-	if (process.argv.length === 1) {
-			return false;
-	}
-
-	const ChildProcess = require('child_process');
-	const path = require('path');
-
-	const appFolder = path.resolve(process.execPath, '..');
-	const rootAtomFolder = path.resolve(appFolder, '..');
-	const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-	const exeName = path.basename(process.execPath);
-
-	const spawn = function(command: any, args: any) {
-			let spawnedProcess, error;
-
-			try {
-					spawnedProcess = ChildProcess.spawn(command, args, {
-							detached: true
-					});
-			} catch (error) {}
-
-			return spawnedProcess;
-	};
-
-	const spawnUpdate = function(args: any[]) {
-			return spawn(updateDotExe, args);
-	};
-
-	const squirrelEvent = process.argv[1];
-	switch (squirrelEvent) {
-			case '--squirrel-install':
-			case '--squirrel-updated':
-					// Optionally do things such as:
-					// - Add your .exe to the PATH
-					// - Write to the registry for things like file associations and
-					//   explorer context menus
-
-					// Install desktop and start menu shortcuts
-					spawnUpdate(['--createShortcut', exeName]);
-
-					setTimeout(application.quit, 1000);
-					return true;
-
-			case '--squirrel-uninstall':
-					// Undo anything you did in the --squirrel-install and
-					// --squirrel-updated handlers
-
-					// Remove desktop and start menu shortcuts
-					spawnUpdate(['--removeShortcut', exeName]);
-
-					setTimeout(application.quit, 1000);
-					return true;
-
-			case '--squirrel-obsolete':
-					// This is called on the outgoing version of your app before
-					// we update to the new version - it's the opposite of
-					// --squirrel-updated
-
-					application.quit();
-					return true;
-	}
-};
 
 let noxuApp: NoxuNoteApp;
 
